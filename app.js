e9b0c454be8f162940db9b3a3e277fb7d08f9e18
Sam Cenzhang <s.cenzhang@gmail.com>
2018-03-04 23:37:33 -0500

    implement passport login

--- app.js
+++ app.js
@@ -4,13 +4,66 @@ const app = express();
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const path = require('path');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const flash = require('connect-flash');
+const session = require('express-session');
 
 mongoose.connect(process.env.DBSTRING);
+app.use(flash());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+
+passport.use(new Strategy(
+  (username, password, done) => {
+    if (password === 'admin') {
+      return done(null, {
+        id: 1,
+        name: username
+      });
+    } else {
+      return done(null, false);
+    }
+
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function (user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
 
 const urlencodedParser = bodyParser.urlencoded({
   extended: false
 });
+
+app.post('/login', urlencodedParser,
+  passport.authenticate('local', {
+    failureRedirect: '/login'
+  }),
+  (req, res) => {
+    console.log(req.user);
+    res.redirect('/');
+  });
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname + '/login.html'));
+
+});
+
 
 app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`));
 // let testUID;
@@ -72,9 +125,12 @@ app.put('/api/stories/:storyID', urlencodedParser, (req, res) => {
     blurb,
     text,
     updated: Date.now()
-  }, {"new": true}, (err, result) => {
-    if (err) {
-      res.status(400).end("unable to do it");
+  }, {
+    "new": true
+  }, (err, result) => {
+    if (err || !result) {
+      console.log(err);
+      res.status(400).end("failed to update");
     } else {
       console.log(result);
       res.status(200).end(JSON.stringify(result));
