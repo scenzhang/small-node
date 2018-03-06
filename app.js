d143e2d0e814584e212a6edc3ce0440c9f378565
Sam Cenzhang <s.cenzhang@gmail.com>
2018-03-05 23:45:23 -0500

    sessions

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const passport = require('passport');
 const Strategy = require('passport-local').Strategy;
 const flash = require('connect-flash');
 const session = require('express-session');
-
+const bcrypt = require('bcrypt');
 mongoose.connect(process.env.DBSTRING);
 app.use(flash());
 app.use(session({
@@ -20,36 +20,57 @@ app.use(session({
 
 passport.use(new Strategy(
   (username, password, done) => {
-    if (password === 'admin') {
-      return done(null, {
-        id: 1,
-        name: username
+    models.User.findOne({username}, (err, user) => {
+      bcrypt.compare(password, user.passwordDigest, (err, res) => {
+        if (res) {
+          return done(null, {
+            username: user.username,
+            id: user._id,
+            email: user.email
+          });
+        } else {
+          return done(null, false);
+        }
       });
-    } else {
-      return done(null, false);
-    }
+    });
+    
 
   }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+const ensureAuthenticated = (req, res, next)=> {
+  if (req.isAuthenticated()) {
+    // req.user is available for use here
+    return next(); }
+
+  // denied. redirect to login
+  res.redirect('/login')
+}
+
 passport.serializeUser(function (user, cb) {
   cb(null, user.id);
 });
 
+
 passport.deserializeUser(function (id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, user);
-  });
+    models.User.findById(id, (err, user) =>{
+      cb(err, user);
+    })
 });
 
 const urlencodedParser = bodyParser.urlencoded({
   extended: false
 });
 
+app.get('/', (req, res) => {
+  res.end(`Welcome, ${req.user ?  req.user.username : 'guest'}`);
+});
+
+app.get('/blah', (req, res) => {
+  res.end(`blah, ${req.session.passport.user}`);
+});
 app.post('/login', urlencodedParser,
   passport.authenticate('local', {
     failureRedirect: '/login'
@@ -64,6 +85,15 @@ app.get('/login', (req, res) => {
 
 });
 
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+})
+
+app.get('/new', urlencodedParser, ensureAuthenticated, (req, res) => {
+  res.end(`new post by ${req.user.username}`);
+})
+
 
 app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`));
 // let testUID;
@@ -73,30 +103,33 @@ app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.
 // });
 // console.log(testUID);
 app.post('/api/users', urlencodedParser, (req, res) => {
-  let newUser = new models.User({
-    username: req.body.username,
-    email: req.body.email,
-    passwordDigest: "placeholder",
-    created: Date.now(),
-    updated: Date.now()
-  });
-  newUser.save(
-    (err) => {
-      if (err) {
-        res.status(400).end(err.errmsg);
-      } else {
-        res.status(200).json(newUser);
-      }
+  bcrypt.hash(req.body.password, 5, (err, hash) => {
+    let newUser = new models.User({
+      username: req.body.username,
+      email: req.body.email,
+      passwordDigest: hash,
+      created: Date.now(),
+      updated: Date.now()
     });
+    newUser.save(
+      (err) => {
+        if (err) {
+          res.status(400).end(err.errmsg);
+        } else {
+          res.status(200).json({username: newUser.username});
+        }
+      });
+    
+  })
 });
 
-app.post('/api/stories', urlencodedParser, (req, res) => {
+app.post('/api/stories', urlencodedParser, ensureAuthenticated, (req, res) => {
   // console.log(testUser);
   let newStory = new models.Story({
     title: req.body.title,
     blurb: req.body.blurb || "",
     text: req.body.body,
-    authorId: req.body.authorId,
+    authorId: req.user.id,
     created: Date.now(),
     updated: Date.now(),
     type: req.body.type
